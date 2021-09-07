a29f372c00deecdd65f57f73cba538659156084f
pinky95123 <pinky95123@gmail.com>
2021-09-08 01:44:46 +0800

    remove authDomain

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBqHWdDg-ijdxQ_d7QOP-fvxvfaCFG5ygU",
-    authDomain: "auth.https://Tiffany0313.github.io/firechat",
+    authDomain: "firechat-37187.firebaseapp.com",
     projectId: "firechat-37187",
     storageBucket: "firechat-37187.appspot.com",
     messagingSenderId: "1029907543038",
     appId: "1:1029907543038:web:73889e7981a440d976c6af",
-    measurementId: "G-VQ7XT8RGFP",
+    measurementId: "G-VQ7XT8RGFP"
 })
 
 const db = firebaseApp.firestore()
