0c1caea2d67a424b727f68424bb7bf6c8f7ec1f3
pinky95123 <pinky95123@gmail.com>
2021-09-07 21:57:30 +0800

    連接firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBqHWdDg-ijdxQ_d7QOP-fvxvfaCFG5ygU",
+    authDomain: "firechat-37187.firebaseapp.com",
+    projectId: "firechat-37187",
+    storageBucket: "firechat-37187.appspot.com",
+    messagingSenderId: "1029907543038",
+    appId: "1:1029907543038:web:73889e7981a440d976c6af",
+    measurementId: "G-VQ7XT8RGFP"
+})
+
+const db = firebaseApp.firestore()
+
+const auth = firebase.auth()
+
+export { db, auth }
